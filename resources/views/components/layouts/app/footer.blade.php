f5debd03d182af7282d19cbbc1dbbd68308ac890
Athen Lee <cupid.jack505@gmail.com>
2024-12-08 18:28:50 -0800

    fix: footer logo modify

--- resources/views/components/layouts/app/footer.blade.php
+++ resources/views/components/layouts/app/footer.blade.php
@@ -3,7 +3,8 @@
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
                 <a href="/" class="flex items-center">
-                    <img src="{{asset(config('app.logo.light') )}}" class="h-6 me-3" alt="Logo" />
+                    <span class="font-black text-3xl text-white">Editnow</span>
+                    <!-- <img src="{{asset(config('app.logo.light') )}}" class="h-6 me-3" alt="Logo" /> -->
                 </a>
             </div>
             <ul class="flex flex-wrap gap-6 text-primary-100 dark:text-gray-400 text-sm mt-2">
@@ -20,29 +21,29 @@
         </div>
         <hr class="my-6 border-primary-300 sm:mx-auto lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-xs text-primary-100 sm:text-center dark:text-gray-400">© {{ date('Y') }} <a href="/" class="hover:underline text-primary-100">{{ config('app.name') }}™</a>. {{ __('All rights reserved.') }}
-          </span>
+            <span class="text-xs text-primary-100 sm:text-center dark:text-gray-400">© {{ date('Y') }} <a href="/" class="hover:underline text-primary-100">{{ config('app.name') }}™</a>. {{ __('All rights reserved.') }}
+            </span>
             <div class="flex gap-3 mt-4 sm:justify-center sm:mt-0">
                 @if (!empty(config('app.social_links.facebook')))
-                    <x-link.social-icon name="facebook" title="{{ __('Facebook page') }}" link="{{config('app.social_links.facebook')}}" class="text-primary-100 border-primary-200 hover:text-primary-50"/>
+                <x-link.social-icon name="facebook" title="{{ __('Facebook page') }}" link="{{config('app.social_links.facebook')}}" class="text-primary-100 border-primary-200 hover:text-primary-50" />
                 @endif
                 @if (!empty(config('app.social_links.instagram')))
-                    <x-link.social-icon name="instagram" title="{{ __('Instagram page') }}" link="{{config('app.social_links.instagram')}}" class="text-primary-100 border-primary-200 hover:text-primary-50"/>
+                <x-link.social-icon name="instagram" title="{{ __('Instagram page') }}" link="{{config('app.social_links.instagram')}}" class="text-primary-100 border-primary-200 hover:text-primary-50" />
                 @endif
                 @if (!empty(config('app.social_links.youtube')))
-                    <x-link.social-icon name="youtube" title="{{ __('YouTube page') }}" link="{{config('app.social_links.youtube')}}" class="text-primary-100 border-primary-200 hover:text-primary-50"/>
+                <x-link.social-icon name="youtube" title="{{ __('YouTube page') }}" link="{{config('app.social_links.youtube')}}" class="text-primary-100 border-primary-200 hover:text-primary-50" />
                 @endif
                 @if (!empty(config('app.social_links.x')))
-                    <x-link.social-icon name="x" title="{{ __('Twitter page') }}" link="{{config('app.social_links.x')}}" class="text-primary-100 border-primary-200 hover:text-primary-50"/>
+                <x-link.social-icon name="x" title="{{ __('Twitter page') }}" link="{{config('app.social_links.x')}}" class="text-primary-100 border-primary-200 hover:text-primary-50" />
                 @endif
                 @if (!empty(config('app.social_links.linkedin')))
-                    <x-link.social-icon name="linkedin" title="{{ __('Linkedin page') }}" link="{{config('app.social_links.linkedin')}}" class="text-primary-100 border-primary-200 hover:text-primary-50"/>
+                <x-link.social-icon name="linkedin" title="{{ __('Linkedin page') }}" link="{{config('app.social_links.linkedin')}}" class="text-primary-100 border-primary-200 hover:text-primary-50" />
                 @endif
                 @if (!empty(config('app.social_links.github')))
-                    <x-link.social-icon name="github" title="{{ __('Github page') }}" link="{{config('app.social_links.github')}}" class="text-primary-100 border-primary-200 hover:text-primary-50"/>
+                <x-link.social-icon name="github" title="{{ __('Github page') }}" link="{{config('app.social_links.github')}}" class="text-primary-100 border-primary-200 hover:text-primary-50" />
                 @endif
                 @if (!empty(config('app.social_links.discord')))
-                    <x-link.social-icon name="discord" title="{{ __('Discord community') }}" link="{{config('app.social_links.discord')}}" class="text-primary-100 border-primary-200 hover:text-primary-50"/>
+                <x-link.social-icon name="discord" title="{{ __('Discord community') }}" link="{{config('app.social_links.discord')}}" class="text-primary-100 border-primary-200 hover:text-primary-50" />
                 @endif
             </div>
         </div>
